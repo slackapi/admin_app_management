6a4ad051874d5072b1b5c27f5f9f5a3157aaa8c8
Gregg Rybicki <grybicki@slack-corp.com>
2019-09-09 16:59:13 -0700

    Adding comments to auth and app files\

--- app.ts
+++ app.ts
@@ -1,16 +1,17 @@
 import express = require('express');
 import bodyParser = require('body-parser');
 const validator = require('./validator');
-import { addTeam, removeTeam, getTeams } from './objects/teams'
-import { addApp, removeApp, getApps } from './objects/apps'
-import { addScope, removeScope, getScopes } from './objects/scopes'
-import { addRequest, removeRequest, manuallyApproveRequest, getRequests, manuallyRejectRequest, pullRequestsFromSlack } from './objects/requests'
-require('./bolt/boltApp');
-require('./auth')
+import { addTeam, removeTeam, getTeams } from './objects/teams' //Imports the file that contains the team logic.
+import { addApp, removeApp, getApps } from './objects/apps' //Imports the file that contains the app logic.
+import { addScope, removeScope, getScopes } from './objects/scopes' //Imports the file that contains the scope logic.
+import { addRequest, removeRequest, manuallyApproveRequest, getRequests, manuallyRejectRequest, pullRequestsFromSlack } from './objects/requests' //Imnports the file that contains the scope logic.
+require('./bolt/boltApp'); //The bolt app file. The Bolt app is what communcicates with Slack. It handles event verification and api calls to the Slack platform.
+require('./auth') //This is used for obtainng the token needed to make api call to Slack at an org level. Using this helper is optional but can be useful if you do not have an OAuth flow setup.
 
 const app: express.Application = express();
 app.use(bodyParser.json());
 
+//Checks for malformed JSON
 app.use(function (error: Error, req: any, res: any, next: any) {
     if (error instanceof SyntaxError) {
         res.send("Bad Payload");
@@ -19,19 +20,27 @@ app.use(function (error: Error, req: any, res: any, next: any) {
     }
 });
 
+//Defines default port to run the APIs from.
 const port = 3000
 
+//Team endpoints for app
 app.post('/add_team', validator.addTeam, addTeam);
 app.get('/get_teams', getTeams);
 app.post('/remove_team', removeTeam);
+
+//Endpoints for managing app approvals
 app.post('/approve_app', validator.approveApp, addApp);
 app.get('/apps', getApps);
 app.post('/restrict_app', validator.restrictApp, addApp);
 app.post('/remove_app', validator.removeApp, removeApp);
+
+//Enpoints for managing scopes
 app.post('/approve_scope', validator.approveScope, addScope);
 app.get('/get_scopes', getScopes);
 app.post('/restrict_scope', validator.rejectScope, addScope);
 app.post('/remove_scope', validator.removeScope, removeScope);
+
+//Endpoints for dealing with requests
 app.post('/pull_requests', pullRequestsFromSlack);
 app.get('/get_requests', getRequests);
 app.post('/remove_request', validator.removeRequest, removeRequest);
--- auth.ts
+++ auth.ts
@@ -1,6 +1,7 @@
 import express = require('express');
 import bodyParser = require('body-parser');
 
+const port = 5000
 const authApp: express.Application = express();
 authApp.use(bodyParser.json());
 
@@ -12,8 +13,7 @@ authApp.use(function (error: Error, req: any, res: any, next: any) {
     }
 });
 
-const port = 5000
-
+//This endpoint should be registered as a redict URL when you create your Slack app. After you authorize your app against your organization, Slack will direct to here passing in the code needed to make the OAuth exchange. You will theen be redirected to the oauth.access page which will provide a JSON payload with your credentials. 
 authApp.get('/redirect/oauth', function (req, res) {
     const client_id = "" //replace with your client_id
     const client_secret = "" //replace with your client_secret
@@ -21,7 +21,6 @@ authApp.get('/redirect/oauth', function (req, res) {
     const url = `https://slack.com/api/oauth.access?code=${code}&client_id=${client_id}&client_secret=${client_secret}`
     res.redirect(url);
 });
-
 
 authApp.get('/', function (req, res) {
     res.send('Try adding a specific method to call');
